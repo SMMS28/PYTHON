c95be12dc10c2a2385245e07d3f4065eaf181e6f
MOHAN MUKUND SAI SUNKARA <116540844+SMMS28@users.noreply.github.com>
2023-10-13 22:21:37 +0530

    Update 12b.py

--- 12b.py
+++ 12b.py
@@ -4,10 +4,7 @@ def draw_square(turtle, size):
     for i in range(4):
         turtle.forward(size)
         turtle.left(90)
-        
         turtle.speed(0)
-
-
 if __name__ == "__main__":
     wn = turtle.Screen()
     wn.bgcolor("black")
@@ -15,8 +12,6 @@ if __name__ == "__main__":
     alex.color("orange")
     alex.pensize(3)
     boxes = 100
-    
-
     for _ in range(boxes):
         draw_square(alex, 200)
         alex.left(360 / boxes)
