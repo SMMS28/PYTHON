ceb2931a7e2be2a2da017bdc5abade613f2eab2c
MOHAN MUKUND SAI SUNKARA <116540844+SMMS28@users.noreply.github.com>
2023-11-01 10:14:19 +0530

    Update 12b1.py

--- 12b1.py
+++ 12b1.py
@@ -3,7 +3,6 @@ import turtle
 turtle.bgcolor("orange")
 turtle.pensize(2)
 turtle.speed(0)
-
 for i in range(6):
       for colours in ["red","blue","green","blue","red","green"]:
                turtle.color(colours)
